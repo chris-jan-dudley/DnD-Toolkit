9226c42685e66078535e870cb2a86f1befaeb34d
Chris <chris.jan.dudley@gmail.com>
2016-09-08 22:19:11 +0100

    First Initiative Cookie

--- jquery.js
+++ jquery.js
@@ -1,22 +1,44 @@
 // JavaScript Document
 
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1	;
+        }
+}
+}
+
 function AddRow()
 {
+	clickCounter();
 	var name = $('#namein').val();
 	var init = $('#initin').val();
 	var hp = $('#hpin').val();
     $('#myTable').find('tbody').append('<tr><td>'+name+'</td><td>'+init+'</td><td><input class="hptab" type="number" value='+hp+'></td></tr>').trigger('update');
+	localStorage.setItem("entry"+String(localStorage.clickcount)+"name",String(name));
+	localStorage.setItem("entry"+String(localStorage.clickcount)+"i",String(init));
+	localStorage.setItem("entry"+String(localStorage.clickcount)+"hp",String(hp));
+
 }
 
 $(document).ready(function() 
     { 
         $("#myTable").tablesorter(); 
+		for (var i=1;i<= Number(localStorage.clickcount);i++){
+		var nam = localStorage.getItem("entry"+String(i)+"name");
+		var int = localStorage.getItem("entry"+String(i)+"i");
+		var hp1 = localStorage.getItem("entry"+String(i)+"hp");
+		$('#myTable').find('tbody').append('<tr><td>'+nam+'</td><td>'+int+'</td><td><input class="hptab" type="number" value='+hp1+'></td></tr>').trigger('update');
+		}
     } 
 ); 
 
  function ClearTab()
  {
 	$('#myTable').find("tr:gt(0)").remove(); 
+	localStorage.clear();
  }
 	
 
